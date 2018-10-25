6dc70f371eab4370b61f9b895d5b4ec4a610f5cf
Glitch (adam-the-house-elf) <none>
2018-10-25 09:06:24 +0000

    Add google search command

--- commands/google.js
+++ commands/google.js
@@ -1,6 +1,38 @@
-module.exports = {
-  commandAliases: ['google'],
-  run: (client, message, args) => {
+let google = require('google');
+let Discord = require('discord.js');
+
+google.resultsPerPage = 5;
+let nextCounter = 0;
+
+exports.run = (client, message, args) => {
+  // Remove "google" from the arguments and convert them into a string
+  args.shift();
+  args = args.join(" ");
+  
+  const embed = new Discord.RichEmbed()
+    .setAuthor("Search for: " + args, "https://images-ext-1.discordapp.net/external/UsMM0mPPHEKn6WMst8WWG9qMCX_A14JL6Izzr47ucOk/http/i.imgur.com/G46fm8J.png")
+    .setColor(0x00AE86);
     
-  }
+  // Google search the args
+  google(args, function (err, res){
+    if (err) console.error(err);
+
+    // Add up to 5 search results to the embed
+    for (var i = 0; i < 5; ++i) {
+      var link = res.links[i];
+      
+      // Skip cards
+      if (link.link === null) {
+        continue;
+      }
+      // Include video-only results
+      if (link.description === "")
+        link.description = "None";
+      
+      embed.addField(link.title + "\n" + link.href, 
+                     link.description, 
+                     false);
+    }
+    message.channel.send({embed});
+  });
 }
