8120c0a66d7b83f7408650df36dfa645bf8b0018
Glitch (adam-the-house-elf) <none>
2018-10-25 12:49:34 +0000

    Add link to results page of google search

--- commands/google.js
+++ commands/google.js
@@ -1,22 +1,22 @@
 let google = require('google');
 let Discord = require('discord.js');
 
-google.resultsPerPage = 5;
-let nextCounter = 0;
-
 exports.run = (client, message, args) => {
   // Convert the search args into a string
   args = args.join(" ");
   
-  const embed = new Discord.RichEmbed()
-    .setAuthor("Search for: " + args, "https://images-ext-1.discordapp.net/external/UsMM0mPPHEKn6WMst8WWG9qMCX_A14JL6Izzr47ucOk/http/i.imgur.com/G46fm8J.png")
-    .setColor(0x00AE86);
+  const embed = new Discord.RichEmbed().setColor(0x00AE86);
     
   // Google search the args
   google(args, function (err, res){
     if (err) console.error(err);
 
+    embed.setAuthor("Search for: " + args, 
+                    "https://images-ext-1.discordapp.net/external/UsMM0mPPHEKn6WMst8WWG9qMCX_A14JL6Izzr47ucOk/http/i.imgur.com/G46fm8J.png",
+                    res.url);
+    
     // Add up to 5 search results to the embed
+    google.resultsPerPage = 5;
     for (var i = 0; i < 5; ++i) {
       var link = res.links[i];
       
